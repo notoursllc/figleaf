7fb77c21f80411c052c3e11fafa6fb0ca1b6c0d6
gbru <gbru@users.noreply.github.com>
2020-11-23 14:43:53 -0800

    feat: Add FormRadio component

---
+++ packages/figleaf/components/form/FormRadio.vue
@@ -0,0 +1,43 @@
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'FormRadio',
+
+    props: {
+        value: {}
+    },
+
+    data: () => ({
+        isChecked: null
+    }),
+
+    watch: {
+        value: {
+            handler: function(newVal) {
+                this.isChecked = newVal;
+            },
+            immediate: true
+        }
+    },
+
+    methods: {
+        emitInput(val) {
+            this.$emit('input', this.isChecked);
+        }
+    }
+});
+</script>
+
+
+<template>
+    <label class="inline-flex items-center">
+        <input
+            type="radio"
+            class="form-radio border-gray-500"
+            v-bind="$attrs"
+            @change="emitInput"
+            v-model="isChecked">
+        <span class="ml-2" v-if="$slots.default"><slot></slot></span>
+    </label>
+</template>
