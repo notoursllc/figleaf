9a7b10e88408b6a845260a710df53e2ec63c3c51
gbru <gbru@users.noreply.github.com>
2020-11-23 19:57:09 -0800

    feat: Add FormInputMoney component

---
+++ packages/figleaf/components/form/FormInputMoney.vue
@@ -0,0 +1,88 @@
+<script>
+import accounting from 'accounting';
+import { CurrencyDirective } from 'vue-currency-input';
+
+
+export default {
+    directives: {
+        currency: CurrencyDirective
+    },
+
+    props: {
+        value: {
+            type: Number,
+            default: 0
+        }
+    },
+
+    data: function() {
+        return {
+            selectedPrice: null
+        };
+    },
+
+    watch: {
+        value: {
+
+            /**
+             * Value is sent as a number (in cents) that needs
+             * to be converted to 'dollars' (divide by 100)
+             */
+            handler(newVal) {
+                this.selectedPrice = newVal ? newVal/100 : 0;
+                // console.log('watch', newVal, this.selectedPrice)
+
+            },
+            immediate: true
+        }
+    },
+
+    methods: {
+
+        /**
+         * Value is emitted as a number (cents)
+         *
+         * @param val String
+         */
+        emitInput() {
+            let clean = 0;
+
+            if(this.selectedPrice) {
+                clean = accounting.toFixed(parseFloat(this.selectedPrice) * 100, 0);
+
+                // accounting returns a string.  This converts back to a float
+                clean = parseFloat(clean);
+            }
+
+            // console.log('MONEY EMIT CLEAN', clean, typeof clean)
+            this.$emit('input', clean);
+        }
+    }
+};
+</script>
+
+
+<template>
+    <input
+        type="text"
+        v-model="selectedPrice"
+        @input="emitInput"
+        class="form-input w-full fig-input-money"
+        :class="inputClassNames"
+        v-currency="{
+            currency: 'USD',
+            locale: 'en-US',
+            valueAsInteger: false,
+            allowNegative: false,
+            distractionFree: true,
+            autoDecimalMode: true,
+            valueRange: { min: 0 },
+            allowNegative: false }">
+</template>
+
+
+<style scoped>
+.fig-input-money {
+    min-width: 70px;
+}
+</style>
--- packages/figleaf/index.js
+++ packages/figleaf/index.js
@@ -2,4 +2,5 @@
 export { default as FigFormCheckbox } from './components/form/FormCheckbox';
 export { default as FigFormGroup } from './components/form/FormGroup';
 export { default as FigFormInput } from './components/form/FormInput';
+export { default as FormInputMoney } from './components/form/FormInputMoney';
 export { default as FigFormTextarea } from './components/form/FormTextarea';
