e8890a2a565dfa8b133f510f9dc0b76bf34a9d3e
gbru <gbru@users.noreply.github.com>
2020-12-06 21:19:28 -0800

    refactor: Th updates

--- packages/figleaf/components/tableSimple/Th.vue
+++ packages/figleaf/components/tableSimple/Th.vue
@@ -35,6 +35,26 @@ export default Vue.extend({
         classNames() {
             const classes = [];
 
+            if(this.tableState.bordered) {
+                classes.push(
+                    'border',
+                    'border-gray-600'
+                );
+            }
+
+            return classes;
+        },
+
+        containerClasses() {
+            const classes = ['p-2', 'flex', 'items-center', 'relative'];
+
+            if(this.sortable) {
+                classes.push(
+                    'cursor-pointer',
+                    'pr-3'
+                );
+            }
+
             if(this.isActive) {
                 classes.push(
                     'bg-gray-200',
@@ -47,23 +67,7 @@ export default Vue.extend({
             }
             else {
                 classes.push(
-                    'text-gray-500',
-                    'border-b',
-                    'border-gray-300'
-                );
-            }
-
-            if(this.sortable) {
-                classes.push(
-                    'cursor-pointer',
-                    'pr-3'
-                );
-            }
-
-            if(this.tableState.bordered) {
-                classes.push(
-                    'border',
-                    'border-gray-600'
+                    'text-gray-500'
                 );
             }
 
@@ -100,31 +104,35 @@ export default Vue.extend({
 
 <template>
     <th
-        class="fig-table-th transition-colors relative font-normal text-gray-600 bg-gray-100 border-b border-gray-300 text-sm text-left px-2 py-1"
+        class="fig-table-th"
         :class="classNames"
         @click="onClick"
         v-bind="$attrs">
-        <slot></slot>
-        <span
-            v-if="sortable"
-            class="fig-table-th-arrow"
-            :class="arrowClasses">
-            <fig-icon
-                class="block"
-                icon="chevron-up"
-                :stroke-width="2"
-                stroke="#737373"
-                :width="16"
-                :height="16" />
-        </span>
+        <div :class="containerClasses">
+            <div class="flex-grow"><slot></slot></div>
+            <div
+                v-if="sortable"
+                class="fig-table-th-arrow"
+                :class="arrowClasses">
+                <fig-icon
+                    class="block"
+                    icon="chevron-up"
+                    :stroke-width="2"
+                    stroke="#737373"
+                    :width="16"
+                    :height="16" />
+            </div>
+        </div>
     </th>
 </template>
 
 
 <style lang="postcss" scoped>
+.fig-table-th {
+    @apply transition-colors relative font-normal text-gray-600 bg-gray-100 text-sm text-left border-r border-b border-gray-300;
+}
 .fig-table-th .fig-table-th-arrow {
     @apply absolute transition-transform duration-500 mt-1;
-    /* top: 50%; */
     right: 8px;
 }
 .fig-table-th .fig-table-th-arrow-down {
