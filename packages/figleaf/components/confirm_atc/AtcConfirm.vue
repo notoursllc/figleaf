bf653a33bf3fdd7a46f3d9ac2aa358c2884588bf
gbru <gbru@users.noreply.github.com>
2021-02-10 20:29:03 -0800

    feat: WIP: Add component AtcConfirm

---
+++ packages/figleaf/components/confirm_atc/AtcConfirm.vue
@@ -0,0 +1,121 @@
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'AtcConfirm',
+
+    props: {
+        centered: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    data() {
+        return {
+            confirmed: false
+        };
+    },
+
+    methods: {
+        show() {
+            document.body.style.overflow = 'hidden'; // prevent body from scrolling too (double scroll bars)
+            this.$root.$emit('fig::atc_confirm::show');
+        },
+
+        hide(e) {
+            document.body.style.overflow = '';
+
+            // adding this component instance on the event so it's data can be
+            // accessed by the event handler
+            e.figConfirm = this;
+
+            this.$emit('hide', e);
+            this.$root.$emit('fig::atc_confirm::hide');
+        },
+
+        onConfirm(e) {
+            this.confirmed = true;
+            this.hide(e);
+        },
+
+        onReject(e) {
+            this.confirmed = false;
+            this.hide(e);
+        }
+    }
+});
+</script>
+
+
+<template>
+    <div>
+        <div
+            class="flex items-center overflow-x-hidden overflow-y-auto fixed top-0 left-0 w-full h-full z-50 outline-none focus:outline-none"
+            v-hotkey="{'esc': onReject}">
+
+            <!--content-->
+            <div class="relative w-full">
+                <div
+                    class="relative max-w-xs' mx-auto border-0 rounded-md shadow-tight bg-white outline-none focus:outline-none">
+
+                    <div class="p-5">
+                        <!--title-->
+                        <div
+                            v-if="$slots.title"
+                            class="pb-2 break-words font-semibold"
+                            :class="{'text-center': centered}"><slot name="title"></slot></div>
+
+                        <!--body-->
+                        <div
+                            class="relative flex-auto text-md text-gray-600 break-words text-center"
+                            :class="{'text-center': centered}"><slot name="message"></slot></div>
+                    </div>
+
+                    <!--footer buttons-->
+                    <div class="footer-container">
+                        <button
+                            type="button"
+                            class="confirm-btn-right hover:bg-gray-200"
+                            ref="btn_confirm_checkout"
+                            @click="onReject"><slot name="cancelLabel">View Cart</slot></button>
+                        <button
+                            type="button"
+                            class="hover:bg-gray-200"
+                            @click="onConfirm"><slot name="checkoutLabel">Checkout</slot></button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- backdrop -->
+        <div class="opacity-25 fixed top-0 left-0 z-40 bg-black h-screen w-screen"></div>
+    </div>
+</template>
+
+
+<style lang="postcss">
+.footer-container {
+    @apply flex items-center border-t border-solid border-gray-300 bg-gray-100 rounded-b-md;
+}
+.footer-container > button {
+    @apply text-center py-3 px-5 border-0;
+    flex-basis:50%;
+}
+.footer-container > button.confirm-btn-right {
+    @apply border-r border-gray-300;
+}
+
+/**
+* Transition
+*/
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.21s;
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
---
+++ packages/figleaf/components/confirm_atc/index.js
@@ -0,0 +1,135 @@
+// this component was influenced by:
+// https://github.com/bootstrap-vue/bootstrap-vue/blob/7ea0cc4a16d27b179eca47d351eaa9fe6fdfd56e/src/components/modal/helpers/bv-modal.js#L85
+
+import Vue from 'vue';
+import isObject from 'lodash.isobject';
+import Confirm from './AtcConfirm.vue';
+
+export default () => {
+
+    if(!isObject(Vue.prototype.$figleaf)) {
+        Vue.prototype.$figleaf = {};
+    }
+
+    Vue.prototype.$figleaf.atcConfirm = (message, config) => {
+        const AtcConfirmComponent = Vue.extend(Confirm);
+
+        const cfg = Object.assign(
+            {},
+            {
+                title: null,
+                checkoutLabel: null,
+                checkoutLabelClass: null,
+                viewCartLabel: null,
+                viewCartLabelClass: null,
+                centered: true
+            },
+            config
+        );
+
+        const confirm = new AtcConfirmComponent({
+            propsData: {
+                centered: cfg.centered
+            },
+
+            mounted() {
+                // Self destruct handler
+                const handleDestroy = () => {
+                    this.$nextTick(() => {
+                        this.$destroy();
+                    });
+                };
+
+                // Self destruct if parent destroyed
+                // this.$parent.$once('hook:destroyed', handleDestroy);
+
+                // Self destruct after hidden
+                this.$once('hidden', handleDestroy);
+
+                // Self destruct on route change
+                if (this.$router && this.$route) {
+                    // Destroy ourselves if route changes
+                    this.$once('hook:beforeDestroy', this.$watch('$router', handleDestroy));
+                }
+
+                this.show();
+
+                if(this.$refs.btn_confirm_checkout) {
+                    this.$refs.btn_confirm_checkout.focus();
+                }
+            },
+
+            destroyed() {
+                if (this.$el && this.$el.parentNode) {
+                    this.$el.parentNode.removeChild(this.$el);
+                }
+            }
+        });
+
+
+        if(cfg.title) {
+            confirm.$slots.title = confirm.$createElement(
+                'div',
+                {
+                    class: 'text-center'
+                },
+                cfg.title
+            );
+        }
+
+        if(cfg.checkoutLabel) {
+            confirm.$slots.checkoutLabel = confirm.$createElement(
+                'div',
+                {
+                    class: cfg.checkoutLabelClass || 'text-blue-600 font-semibold'
+                },
+                cfg.checkoutLabel
+            );
+        }
+
+        if(cfg.viewCartLabel) {
+            confirm.$slots.viewCartLabel = confirm.$createElement(
+                'div',
+                {
+                    class: cfg.viewCartLabelClass || 'text-blue-600 font-semibold'
+                },
+                cfg.viewCartLabel
+            );
+        }
+
+        if(message) {
+            confirm.$slots.message = confirm.$createElement(
+                'div',
+                {
+                },
+                Array.isArray(message) ? message : [message]
+            );
+        }
+
+        // Return a promise that resolves when hidden, or rejects on destroyed
+        return new Promise((resolve, reject) => {
+            let resolved = false;
+
+            confirm.$once('hook:destroyed', () => {
+                if (!resolved) {
+                    reject(new Error('Figleaf AtcConfirm was destroyed before resolve'));
+                }
+            });
+
+            confirm.$on('hide', confirmEvt => {
+                if (!confirmEvt.defaultPrevented) {
+                    resolved = true;
+                    resolve(confirmEvt.figConfirm.confirmed);
+                    confirm.$destroy();
+                }
+            });
+
+            // Create a mount point (a DIV) and mount the msgBo which will trigger it to show
+            const div = document.createElement('div');
+            document.body.appendChild(div);
+            confirm.$mount(div);
+        });
+
+    };
+
+};
