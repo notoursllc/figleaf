8b04746bdcea84f0f3bcce36a0a8cfb2cbe623dc
gbru <gbru@users.noreply.github.com>
2021-04-27 21:57:41 -0700

    feat: Add CartCtaLayout component

---
+++ packages/figleaf/components/layouts/cartCta/CartCtaLayout.stories.js
@@ -0,0 +1,29 @@
+import FigCartCtaLayout from './CartCtaLayout.vue';
+
+export default {
+    title: 'Components/Layouts/CartCtaLayout',
+
+    component: FigCartCtaLayout,
+
+    argTypes: {
+
+    }
+};
+
+const Template = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: {
+        FigCartCtaLayout
+    },
+    template: `
+        <fig-cart-cta-layout v-bind="$props">
+            <div slot="left">left side</div>
+            <div slot="right">right side</div>
+        </fig-cart-cta-layout>
+    `
+});
+
+export const CartCtaLayout = Template.bind({});
+CartCtaLayout.args = {
+};
+
---
+++ packages/figleaf/components/layouts/cartCta/CartCtaLayout.vue
@@ -0,0 +1,26 @@
+
+<script>
+export default {
+    name: 'CartCtaLayout',
+
+    props: {
+        shadow: {
+            type: Boolean
+        }
+    },
+
+    computed: {
+
+    }
+};
+</script>
+
+
+<template>
+    <section class="container mx-auto">
+        <div class="flex flex-col md:flex-row items-start items-stretch mx-auto">
+            <div class="flex-grow md:pr-8 align-top w-full md:w-2/3"><slot name="left"></slot></div>
+            <div class="flex-shrink-0 align-top w-full md:w-1/3"><slot name="right"></slot></div>
+        </div>
+    </section>
+</template>
