970f6d63590574c17d907d1dba9891385561df28
gbru <gbru@users.noreply.github.com>
2021-08-02 21:35:55 -0700

    refactor: Update FigJsonTreeView

--- packages/figleaf/components/jsonTreeView/JsonTreeView.vue
+++ packages/figleaf/components/jsonTreeView/JsonTreeView.vue
@@ -5,17 +5,11 @@ import JsonTree from 'vue-json-tree';
 Vue.component('json-tree', JsonTree);
 
 export default {
-    name: 'JsonTreeView',
-
-    render(h) {
-        return h(
-            JsonTree,
-            {
-                props: this.$props,
-                on: this.$listeners,
-                attrs: this.$attrs
-            }
-        );
-    }
+    name: 'JsonTreeView'
 };
 </script>
+
+
+<template>
+    <json-tree v-bind="$attrs" />
+</template>
