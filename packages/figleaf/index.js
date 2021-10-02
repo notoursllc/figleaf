609737fde53082c8c45778658718b84b98e63eeb
gbru <gbru@users.noreply.github.com>
2021-10-01 19:44:31 -0700

    feat: Exporting FigDataToggler

--- packages/figleaf/index.js
+++ packages/figleaf/index.js
@@ -1,132 +1,135 @@
-export { default as FigBadge } from './components/Badge';
-export { default as FigCountry } from './components/Country';
-export { default as FigIconLabel } from './components/iconLabel/IconLabel';
+export { default as FigBadge } from './components/Badge.vue';
+export { default as FigCountry } from './components/Country.vue';
+export { default as FigIconLabel } from './components/iconLabel/IconLabel.vue';
 
 // address
-export { default as FigAddress } from './components/address/Address';
+export { default as FigAddress } from './components/address/Address.vue';
 
 // address form
-export { default as FigAddressForm } from './components/addressForm/AddressForm';
+export { default as FigAddressForm } from './components/addressForm/AddressForm.vue';
 
 // buttons
-export { default as FigButton } from './components/button/Button';
-export { default as FigButtonFab } from './components/ButtonFab';
+export { default as FigButton } from './components/button/Button.vue';
+export { default as FigButtonFab } from './components/ButtonFab.vue';
 
 // content
-export { default as FigContent } from './components/content/Content';
+export { default as FigContent } from './components/content/Content.vue';
+
+// data toggler
+export { default as FigDataToggler } from './components/dataToggler/DataToggler.vue';
 
 // divider
-export { default as FigDivider } from './components/divider/Divider';
+export { default as FigDivider } from './components/divider/Divider.vue';
 
 // dropdown
-export { default as FigDropdown } from './components/dropdown/Dropdown';
-export { default as FigDropdownButton } from './components/dropdown/DropdownButton';
-export { default as FigDropdownDivider } from './components/dropdown/DropdownDivider';
+export { default as FigDropdown } from './components/dropdown/Dropdown.vue';
+export { default as FigDropdownButton } from './components/dropdown/DropdownButton.vue';
+export { default as FigDropdownDivider } from './components/dropdown/DropdownDivider.vue';
 
 // grid
-export { default as FigCol } from './components/grid/Col';
-export { default as FigRow } from './components/grid/Row';
+export { default as FigCol } from './components/grid/Col.vue';
+export { default as FigRow } from './components/grid/Row.vue';
 
 // header
-export { default as FigHeader } from './components/header/Header';
+export { default as FigHeader } from './components/header/Header.vue';
 
 // icon
-export { default as FigIcon } from './components/icon/FigIcon';
-export { default as FigIconSprite } from './components/icon/FigIconSprite';
-export { default as FigVictoryIcon } from './components/icon/VictoryIcon';
-export { default as FigCreditCardIcon } from './components/icon/creditCard/CreditCard';
+export { default as FigIcon } from './components/icon/FigIcon.vue';
+export { default as FigIconSprite } from './components/icon/FigIconSprite.vue';
+export { default as FigVictoryIcon } from './components/icon/VictoryIcon.vue';
+export { default as FigCreditCardIcon } from './components/icon/creditCard/CreditCard.vue';
 
 // layouts
-export { default as FigCartCtaLayout } from './layouts/cartCta/CartCtaLayout';
-export { default as FigProductDetailsLayout } from './layouts/productDetailsLayout/ProductDetailsLayout';
-export { default as FigSpecLayout } from './layouts/spec/SpecLayout';
-export { default as FigSpec } from './layouts/spec/Spec';
+export { default as FigCartCtaLayout } from './layouts/cartCta/CartCtaLayout.vue';
+export { default as FigProductDetailsLayout } from './layouts/productDetailsLayout/ProductDetailsLayout.vue';
+export { default as FigSpecLayout } from './layouts/spec/SpecLayout.vue';
+export { default as FigSpec } from './layouts/spec/Spec.vue';
 
 // label value group
-export { default as FigLabelValueGroup } from './components/labelValueGroup/LabelValueGroup';
-export { default as FigLabelValue } from './components/labelValueGroup/LabelValue';
+export { default as FigLabelValueGroup } from './components/labelValueGroup/LabelValueGroup.vue';
+export { default as FigLabelValue } from './components/labelValueGroup/LabelValue.vue';
 
 // message
-export { default as FigMessage } from './components/message/Message';
+export { default as FigMessage } from './components/message/Message.vue';
 
 // modal
-export { default as FigModal } from './components/modal/Modal';
+export { default as FigModal } from './components/modal/Modal.vue';
 
 // money
-export { default as FigMoney } from './components/money/Money';
+export { default as FigMoney } from './components/money/Money.vue';
 
 // operations dropdown
-export { default as FigOperationsDropdown } from './components/operationsDropdown/OperationsDropdown';
+export { default as FigOperationsDropdown } from './components/operationsDropdown/OperationsDropdown.vue';
 
 // pagination
-export { default as FigPaginationBar } from './components/pagination/pagination-bar/PaginationBar';
-export { default as FigPaginator } from './components/pagination/paginator/Paginator';
+export { default as FigPaginationBar } from './components/pagination/pagination-bar/PaginationBar.vue';
+export { default as FigPaginator } from './components/pagination/paginator/Paginator.vue';
 
 // payment type chooser
-export { default as FigPaymentTypeChooser } from './components/paymentTypeChooser/PaymentTypeChooser';
+export { default as FigPaymentTypeChooser } from './components/paymentTypeChooser/PaymentTypeChooser.vue';
 
 // product grid
-export { default as FigProductGrid } from './components/productGrid/ProductGrid';
+export { default as FigProductGrid } from './components/productGrid/ProductGrid.vue';
 
 // popover
-export { default as FigPopover } from './components/popover/Popover';
-export { default as FigPopoverDivider } from './components/popover/PopoverDivider';
-export { default as FigPopConfirm } from './components/popConfirm/PopConfirm';
+export { default as FigPopover } from './components/popover/Popover.vue';
+export { default as FigPopoverDivider } from './components/popover/PopoverDivider.vue';
+export { default as FigPopConfirm } from './components/popConfirm/PopConfirm.vue';
 
 // overlay
-export { default as FigOverlay } from './components/overlay/Overlay';
-export { default as FigSpinner } from './components/spinner/Spinner';
+export { default as FigOverlay } from './components/overlay/Overlay.vue';
+export { default as FigSpinner } from './components/spinner/Spinner.vue';
 
 // signpost
-export { default as FigSignpost } from './components/signpost/Signpost';
+export { default as FigSignpost } from './components/signpost/Signpost.vue';
 
 // slideover
-export { default as FigSlideover } from './components/slideover/Slideover';
+export { default as FigSlideover } from './components/slideover/Slideover.vue';
 
 // stock level warning
-export { default as FigStockLevelWarning} from './components/stockLevelWarning/StockLevelWarning';
+export { default as FigStockLevelWarning} from './components/stockLevelWarning/StockLevelWarning.vue';
 
 // stripe
-export { default as FigStripeForm } from './components/stripeForm/StripeForm';
+export { default as FigStripeForm } from './components/stripeForm/StripeForm.vue';
 
 // tag
-export { default as FigTag } from './components/tag/Tag';
-export { default as FigBooleanTag } from './components/tag/BooleanTag';
+export { default as FigTag } from './components/tag/Tag.vue';
+export { default as FigBooleanTag } from './components/tag/BooleanTag.vue';
 
 // table
-export { default as FigTableSimple } from './components/tableSimple/TableSimple';
-export { default as FigTd } from './components/tableSimple/Td';
-export { default as FigTh } from './components/tableSimple/Th';
-export { default as FigTrNoResults } from './components/tableSimple/TrNoResults';
+export { default as FigTableSimple } from './components/tableSimple/TableSimple.vue';
+export { default as FigTd } from './components/tableSimple/Td.vue';
+export { default as FigTh } from './components/tableSimple/Th.vue';
+export { default as FigTrNoResults } from './components/tableSimple/TrNoResults.vue';
 
 // texas toast
-export { default as FigTexasToast } from './components/texasToast/TexasToast';
+export { default as FigTexasToast } from './components/texasToast/TexasToast.vue';
 
 // text card
-export { default as FigTextCard } from './components/textCard/TextCard';
+export { default as FigTextCard } from './components/textCard/TextCard.vue';
 
 // tooltip
-export { default as FigTooltip } from './components/tooltip/Tooltip';
-export { default as FigTooltipDivider } from './components/tooltip/TooltipDivider';
+export { default as FigTooltip } from './components/tooltip/Tooltip.vue';
+export { default as FigTooltipDivider } from './components/tooltip/TooltipDivider.vue';
 
 // form
-export { default as FigFormCheckbox } from './components/form/checkbox/FormCheckbox';
-export { default as FigFormGroup } from './components/form/group/FormGroup';
-export { default as FigFormInput } from './components/form/text/FormText';
-export { default as FigFormInputDate } from './components/form/FormInputDate';
-export { default as FigFormInputEndcapper } from './components/form/FormInputEndcapper';
-export { default as FigFormInputFileButton } from './components/form/FormInputFileButton';
-export { default as FigFormInputMoney } from './components/form/money/FormInputMoney';
-export { default as FigFormInputNumber } from './components/form/number/FormInputNumber';
-export { default as FigFormInputToggle } from './components/form/FormInputToggle';
-export { default as FigFormRadio } from './components/form/radio/FormRadio';
-export { default as FigFormSelect } from './components/form/select/FormSelect';
-export { default as FigFormSelectBitwise } from './components/form/selectBitwise/FormSelectBitwise';
-export { default as FigFormSelectNative } from './components/form/selectNative/FormSelectNative';
-export { default as FigFormTextarea } from './components/form/textarea/FormTextarea';
+export { default as FigFormCheckbox } from './components/form/checkbox/FormCheckbox.vue';
+export { default as FigFormGroup } from './components/form/group/FormGroup.vue';
+export { default as FigFormInput } from './components/form/text/FormText.vue';
+export { default as FigFormInputDate } from './components/form/FormInputDate.vue';
+export { default as FigFormInputEndcapper } from './components/form/FormInputEndcapper.vue';
+export { default as FigFormInputFileButton } from './components/form/FormInputFileButton.vue';
+export { default as FigFormInputMoney } from './components/form/money/FormInputMoney.vue';
+export { default as FigFormInputNumber } from './components/form/number/FormInputNumber.vue';
+export { default as FigFormInputToggle } from './components/form/FormInputToggle.vue';
+export { default as FigFormRadio } from './components/form/radio/FormRadio.vue';
+export { default as FigFormSelect } from './components/form/select/FormSelect.vue';
+export { default as FigFormSelectBitwise } from './components/form/selectBitwise/FormSelectBitwise.vue';
+export { default as FigFormSelectNative } from './components/form/selectNative/FormSelectNative.vue';
+export { default as FigFormTextarea } from './components/form/textarea/FormTextarea.vue';
 
 // size buttons
-export { default as FigSizeButtons } from './components/sizeButtons/SizeButtons';
+export { default as FigSizeButtons } from './components/sizeButtons/SizeButtons.vue';
 
-export { default as FigSelectCountry } from './components/selectCountry/SelectCountry';
-export { default as FigSelectStateProvince } from './components/selectStateProvince/SelectStateProvince';
+export { default as FigSelectCountry } from './components/selectCountry/SelectCountry.vue';
+export { default as FigSelectStateProvince } from './components/selectStateProvince/SelectStateProvince.vue';
