a7d5dde60eca0eab303dd5e89a3e932ede74a0f5
gbru <gbru@users.noreply.github.com>
2021-10-01 19:16:32 -0700

    feat: Adding @vue/composition-api

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,11 @@
+// import Vue from 'vue'
+// import VueCompositionAPI from '@vue/composition-api'
+
 import i18n from './i18n';
 import { addDecorator } from '@storybook/vue';
 import Canvas from './components/Canvas.vue';
+
+// Vue.use(VueCompositionAPI);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
