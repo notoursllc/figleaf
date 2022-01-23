9960bd4ce39582443ec8e9a32c3bb2f523cbc1e4
gbru <gbru@users.noreply.github.com>
2022-01-22 18:09:27 -0800

    refactor: CSS width adjustmenbt

--- packages/figleaf/components/productGrid/ProductGrid.vue
+++ packages/figleaf/components/productGrid/ProductGrid.vue
@@ -17,7 +17,7 @@ export default {
         <div
             v-for="(product, idx) in products"
             :key="idx"
-            class="my-2 px-2 w-full sm:w-1/2 lg:w-1/3 xl:my-3 xl:px-3">
+            class="my-2 px-2 w-1/2 md:w-1/3 xl:my-3 xl:px-3">
             <slot v-bind:data="{ product: product, index: idx }" />
         </div>
     </div>
