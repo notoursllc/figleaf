cfac714ab9b8d2a98959666fe1f3c1b148577d3a
gbru <gbru@users.noreply.github.com>
2022-08-04 23:50:39 -0700

    fix: Headroom component needs to have one child element
    in order to set the height properly

--- packages/figleaf/components/header/Header.vue
+++ packages/figleaf/components/header/Header.vue
@@ -74,92 +74,94 @@ export default {
 
 <template>
     <headroom :disabled="inCheckout" :zIndex="10">
-        <div class="fig-header-top">
-            <fig-content nopad><slot name="top" /></fig-content>
-        </div>
+        <header role="banner">
+            <div class="fig-header-top">
+                <fig-content nopad><slot name="top" /></fig-content>
+            </div>
 
-        <header role="banner" class="fig-header h-12 md:h-16 flex items-center relative p-0 w-full">
-            <fig-content nopad class="flex flex-row items-center w-full px-4">
-                <div>
-                    <slot name="logo" />
-                </div>
+            <div class="fig-header h-12 md:h-16 flex items-center relative p-0 w-full">
+                <fig-content nopad class="flex flex-row items-center w-full px-4">
+                    <div>
+                        <slot name="logo" />
+                    </div>
 
-                <!-- middle -->
-                <div class="flex flex-no-wrap grow items-center text-base justify-center">
-                    <template v-if="!inCheckout">
-                        <fig-icon
-                            icon="menu-2"
-                            :width="30"
-                            :height="30"
-                            class="block md:hidden cursor-pointer"
-                            @click="emitSidebarOpen" />
+                    <!-- middle -->
+                    <div class="flex flex-no-wrap grow items-center text-base justify-center">
+                        <template v-if="!inCheckout">
+                            <fig-icon
+                                icon="menu-2"
+                                :width="30"
+                                :height="30"
+                                class="block md:hidden cursor-pointer"
+                                @click="emitSidebarOpen" />
 
-                        <div class="hidden md:block">
-                            <slot name="middle" />
-                        </div>
-                    </template>
-
-                    <!-- checkout label and popover -->
-                    <template v-else>
-                        <div class="flex items-center">
-                            <div class="pr-1">
-                                <fig-icon
-                                    icon="lock"
-                                    class="vam"
-                                    :width="22"
-                                    :height="22" />
+                            <div class="hidden md:block">
+                                <slot name="middle" />
                             </div>
+                        </template>
 
-                            <div class="pr-1 text-lg lg:text-xl">{{ $t('Checkout') }}</div>
-
-                            <fig-popover
-                                placement="top"
-                                @visible="onPopoverVisible"
-                                :ref="popoverRef">
-
-                                <div
-                                    slot="toggler"
-                                    class="whitespace-nowrap text-lg lg:text-xl cursor-pointer">(<a class="text-blue-500">{{ numCartItems }}&nbsp;{{ $tc('item_items', numCartItems) }}</a>)</div>
-
-                                <div class="p-2 text-center">{{ $t('Return to your Shopping Cart?') }}</div>
-
-                                <div
-                                    slot="footer"
-                                    class="flex items-center justify-center">
-                                    <fig-button
-                                        variant="plain"
-                                        size="sm"
-                                        @click="hidePopover"
-                                        class="mr-1"
-                                        :ref="popoverCancelButtonRef">{{ $t('cancel') }}</fig-button>
-
-                                    <fig-button
-                                        variant="primary"
-                                        size="sm"
-                                        @click="onReturnToCartClick">{{ $t('Yes, return to cart') }}</fig-button>
+                        <!-- checkout label and popover -->
+                        <template v-else>
+                            <div class="flex items-center">
+                                <div class="pr-1">
+                                    <fig-icon
+                                        icon="lock"
+                                        class="vam"
+                                        :width="22"
+                                        :height="22" />
                                 </div>
-                            </fig-popover>
-                        </div>
-                    </template>
-                </div>
 
-                <!-- left -->
-                <div v-if="!inCheckout">
-                    <button
-                        type="button"
-                        class="cart-button relative p-0 m-0 mt-2 bg-transparent border-0 mr-3 lg:mr-0"
-                        @click="emitCartClick">
-                        <fig-icon
-                            icon="cart"
-                            :width="27"
-                            :height="27"
-                            :stroke-width="1.5" />
-                        <fig-badge
-                            :variant="numCartItems ? 'success' : 'light'"
-                            size="sm">{{ numCartItems }}</fig-badge>
-                    </button>
-                </div>
-            </fig-content>
+                                <div class="pr-1 text-lg lg:text-xl">{{ $t('Checkout') }}</div>
+
+                                <fig-popover
+                                    placement="top"
+                                    @visible="onPopoverVisible"
+                                    :ref="popoverRef">
+
+                                    <div
+                                        slot="toggler"
+                                        class="whitespace-nowrap text-lg lg:text-xl cursor-pointer">(<a class="text-blue-500">{{ numCartItems }}&nbsp;{{ $tc('item_items', numCartItems) }}</a>)</div>
+
+                                    <div class="p-2 text-center">{{ $t('Return to your Shopping Cart?') }}</div>
+
+                                    <div
+                                        slot="footer"
+                                        class="flex items-center justify-center">
+                                        <fig-button
+                                            variant="plain"
+                                            size="sm"
+                                            @click="hidePopover"
+                                            class="mr-1"
+                                            :ref="popoverCancelButtonRef">{{ $t('cancel') }}</fig-button>
+
+                                        <fig-button
+                                            variant="primary"
+                                            size="sm"
+                                            @click="onReturnToCartClick">{{ $t('Yes, return to cart') }}</fig-button>
+                                    </div>
+                                </fig-popover>
+                            </div>
+                        </template>
+                    </div>
+
+                    <!-- left -->
+                    <div v-if="!inCheckout">
+                        <button
+                            type="button"
+                            class="cart-button relative p-0 m-0 mt-2 bg-transparent border-0 mr-3 lg:mr-0"
+                            @click="emitCartClick">
+                            <fig-icon
+                                icon="cart"
+                                :width="27"
+                                :height="27"
+                                :stroke-width="1.5" />
+                            <fig-badge
+                                :variant="numCartItems ? 'success' : 'light'"
+                                size="sm">{{ numCartItems }}</fig-badge>
+                        </button>
+                    </div>
+                </fig-content>
+            </div>
         </header>
     </headroom>
 </template>
