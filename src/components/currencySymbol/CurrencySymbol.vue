8809e0de99856d602d75be0c01c8496b459a0757
gbruins <gbruins@users.noreply.github.com>
2023-06-24 22:58:51 -0700

    fix: Should not have div wrapper

--- src/components/currencySymbol/CurrencySymbol.vue
+++ src/components/currencySymbol/CurrencySymbol.vue
@@ -20,13 +20,11 @@ const props = defineProps({
 
 
 <template>
-    <div>
-        <i18n-n :tag="tag" :value="0" :format="{ key: 'currency', currency: currency }">
-            <template v-slot:currency="slotProps">{{ slotProps.currency }}</template>
-            <template v-slot:integer="slotProps"><!-- --></template> <!-- slotProps.integer -->
-            <template v-slot:group="slotProps"><!-- --></template> <!-- slotProps.group -->
-            <template v-slot:decimal="slotProps"><!-- --></template> <!-- slotProps.decimal -->
-            <template v-slot:fraction="slotProps"><!-- --></template> <!-- slotProps.fraction -->
-        </i18n-n>
-    </div>
+    <i18n-n :tag="tag" :value="0" :format="{ key: 'currency', currency: currency }">
+        <template v-slot:currency="slotProps">{{ slotProps.currency }}</template>
+        <template v-slot:integer="slotProps"><!-- --></template> <!-- slotProps.integer -->
+        <template v-slot:group="slotProps"><!-- --></template> <!-- slotProps.group -->
+        <template v-slot:decimal="slotProps"><!-- --></template> <!-- slotProps.decimal -->
+        <template v-slot:fraction="slotProps"><!-- --></template> <!-- slotProps.fraction -->
+    </i18n-n>
 </template>
