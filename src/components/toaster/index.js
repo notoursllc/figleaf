4c0de3645215fe97da0cdad2882af19e2ba48592
gbruins <gbruins@users.noreply.github.com>
2023-06-25 14:56:49 -0700

    refactor: Export toast methods as default

--- src/components/toaster/index.js
+++ src/components/toaster/index.js
@@ -1,33 +1,35 @@
 import { methods } from './toaster.js';
 
-export const toast = (toastConfig) => {
-    return methods.addToast(
-        Object.assign(
-            {},
-            {
-                variant: 'info',
-                title: null,
-                text: null,
-                closable: true,
-                timeout: 0
-            },
-            toastConfig
-        )
-    );
-}
+export default {
+    toast(toastConfig) {
+        return methods.addToast(
+            Object.assign(
+                {},
+                {
+                    variant: 'info',
+                    title: null,
+                    text: null,
+                    closable: true,
+                    timeout: 0
+                },
+                toastConfig
+            )
+        );
+    },
 
-export const successToast = (toastConfig) => {
-    return toast(
-        Object.assign({}, toastConfig, { variant: 'success', timeout: 5000 })
-    );
-}
+    successToast(toastConfig) {
+        return toast(
+            Object.assign({}, toastConfig, { variant: 'success', timeout: 5000 })
+        );
+    },
 
-export const errorToast = (toastConfig) => {
-    return toast(
-        Object.assign({}, toastConfig, { variant: 'error' })
-    );
-}
+    errorToast(toastConfig) {
+        return toast(
+            Object.assign({}, toastConfig, { variant: 'error' })
+        );
+    },
 
-export const clearToasts = () => {
-    return methods.removeAllToasts();
+    clearToasts() {
+        return methods.removeAllToasts();
+    }
 }
