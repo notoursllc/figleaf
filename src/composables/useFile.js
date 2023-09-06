2cdf52e0d0a0cf015ba68934b0ef34f752a7f6af
gbruins <gbruins@users.noreply.github.com>
2023-09-05 22:38:36 -0700

    feat: Add composable FigUseFile

---
+++ src/composables/useFile.js
@@ -0,0 +1,33 @@
+export default function useFile() {
+
+    /**
+     * Checks to see if every File in FileList is of the accepted file type
+     *
+     * @param {*} FileList
+     * @param {*} accept - array of accepted file types, e.g. ['image/png', 'image/jpeg', 'image/gif']
+     *
+     * https://developer.mozilla.org/en-US/docs/Web/API/FileList
+     * @returns boolean
+     */
+    function filesAreAcceptedType(FileList, accept) {
+        if (!Array.isArray(accept) || accept.length === 0) {
+            return true;
+        }
+
+        const acceptedTypes = accept.map((type) => type.trim());
+        let isAcceptedType = true;
+
+        for (let i=0; i<FileList.length; i++) {
+            if (!acceptedTypes.includes(FileList[i].type)) {
+                isAcceptedType = false;
+            }
+        }
+
+        return isAcceptedType;
+    }
+
+    return {
+        filesAreAcceptedType
+    }
+
+}
